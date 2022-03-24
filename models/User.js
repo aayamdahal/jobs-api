91eaa316f11cc3407e4b39d8d420182b4d66cb1a
Aayam <getaayam@gmail.com>
2022-03-25 00:33:37 +0545

    add password expires.models/User.js

--- models/User.js
+++ models/User.js
@@ -30,7 +30,7 @@ UserSchema.pre("save", async function () {
   // this meaning this doucment
   this.password = await bcrypt.hash(this.password, salt);
 });
-
+  
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
