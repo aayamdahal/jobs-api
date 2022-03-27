8fd7cd1ee19c2e54cf043d3d08d4631e967c688d
Aayam <getaayam@gmail.com>
2022-03-28 00:11:32 +0545

    add uthenticateUser middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
+const authenticateUser = require("./middleware/authentication");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -17,12 +18,12 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
   res.send("jobs api");
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
