3dfc64aa35ae2866c811469c398c640823467789
Aayam <getaayam@gmail.com>
2022-03-29 09:50:02 +0545

    refactor authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,12 +12,16 @@ const auth = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // alternate way
+    // const user = User.findById(payload.id).select("-password");
     // attach the user to the jobs route
+
     req.user = { userId: payload.userId, name: payload.name };
+    next();
   } catch (error) {
-    throw new UnautheniticatedError("Authentication Invalid");
+    throw new UnauthenticatedError("Authentication Invalid");
   }
 };
 
-
-module.exports = auth
+module.exports = auth;
