41b716a645f98e668499a6daf8cc14a45e292f14
Aayam <getaayam@gmail.com>
2022-03-31 07:45:51 +0545

    add helmet,cors,xss and rate limiter

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ const authenticateUser = require("./middleware/authentication");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 // connect db
 const connectDB = require("./db/connect");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+// extra security packages
 
 // routes
 app.get("/", (req, res) => {
